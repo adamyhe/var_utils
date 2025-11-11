098e9352bb3c3af3f5217c1f694312a4f7ee4c90
Adam Youlin He <ayh8@cornell.edu>
2025-11-11 12:06:12 -0800

    added tool to cleanup snp files for deeplift analysis

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This tool uses the Ensembl REST API to fetch variants in LD with a provided
 list of variants based on 1K Genomes populations. It can launch multiple API calls at once to reduce runtime. NOTE: you should probably use a relatively small number still (e.g., <0.8) to avoid getting in trouble with Ensembl sysadmins.
 
 ```bash
+ld_fetch -h
 ld_fetch --input rsids.txt --output rsids_ld.txt [--pop CEU --threshold 0.8 --metric d_prime/r2 --wsize 200 --nthreads 1 --verbose]
 ```
 
@@ -22,5 +23,15 @@ ld_fetch --input rsids.txt --output rsids_ld.txt [--pop CEU --threshold 0.8 --me
 This tool looks up coordinates, alleles, and allele frequency data from the UCSC dbSNP tables (e.g., https://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/snp151.txt.gz) for a given list of rsIDs.
 
 ```bash
+lookup_dbsnp -h
 lookup_dbsnp --input rsids.txt --dbsnp snp151.txt.gz --output snp_data.tsv.gz [--tmp tmp_file.txt]
+```
+
+## cleanup_snps
+
+This tool cleans up a tsv containing SNPs of interest (excludes all SNPs that contain non-ACGT characters nearby and will cause edge run-over).
+
+```bash
+cleanup_snps -h
+cleanup_snps --input snps.tsv --fasta genome.fna --output [--in_window 2114 --allowed_chars ACGT --verbose]
 ```
---
+++ src/cleanup_snps.py
@@ -0,0 +1,66 @@
+# cleanup_snps.py
+# Adam He <adamyhe@gmail.com>
+
+"""
+Tool to clean up a tsv containing SNPs of interest (such as one produced by
+lookup_dbsnp). Excludes all SNPs that contain non-ACGT characters nearby
+(default = 2114 bases).
+"""
+
+import argparse
+
+import pandas as pd
+import pyfastx
+import tqdm
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        required=True,
+        help="Input SNPs tsv. Required columns: chrom, pos",
+    )
+    parser.add_argument("-f", "--fasta", type=str, required=True)
+    parser.add_argument("-o", "--output", type=str, required=True)
+    parser.add_argument("-w", "--in_window", type=int, default=2114)
+    parser.add_argument("-a", "--allowed_chars", type=str, default="ACGT")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+
+    # Read in sequences around SNPs
+    snps = pd.read_csv(args.snps_tsv, sep="\t")
+    coords = snps[["chrom", "pos"]].copy()
+    coords["end"] = coords["pos"] + 1
+    fa = pyfastx.Fasta(args.fa_fname)
+    wholesome = []
+    for row in tqdm.tqdm(
+        snps.itertuples(), total=snps.shape[0], disable=not args.verbose
+    ):
+        chrom = row[1]
+        center = row[2]
+        start = max(0, center - args.in_window // 2)
+        end = center + args.in_window // 2 - 1  # pyfastx includes the end
+        seq = fa.fetch(chrom, (start, end)).upper()
+        is_wholesome = (
+            all([c in args.allowed_chars for c in seq]) and len(seq) == args.in_window
+        )
+        wholesome.append(is_wholesome)
+
+    # Dump cleaned SNPs to tsv
+    print(
+        "Filtered out {} due to disallowed characters or length != {}".format(
+            snps.shape[0] - sum(wholesome), args.in_window
+        )
+    )
+    snps = snps[wholesome]
+    snps.reset_index(drop=True, inplace=True)
+    snps.to_csv(args.out_tsv, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
